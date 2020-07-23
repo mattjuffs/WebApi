820a07630c1229521770efbcda28f78149468e14
HOTH\Matt Juffs <matthew.juffs@gmail.com>
2020-07-23 16:43:23 +0100

    Added EntityFramework data context

---
+++ Data/DataContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Reflection;
+
+using Microsoft.Extensions.Configuration;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Data
+{
+    public class DataContext : DbContext, IDbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        public IModel GetModel()
+        {
+            return Model;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Entities.Account>().ToTable("tbl_Account");
+            modelBuilder.Entity<Entities.MeterReading>().ToTable("tbl_MeterReading");
+        }
+    }
+}
---
+++ Data/IDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Data
+{
+    public interface IDbContext : IDisposable
+    {
+        DbSet<T> Set<T>() where T : class;
+        int SaveChanges();
+        IModel GetModel();
+        EntityEntry<TEntity> Entry<TEntity>(TEntity entity) where TEntity : class;
+    }
+}
