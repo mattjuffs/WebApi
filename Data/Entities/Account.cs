eaddc7de913b8bdb5a063386bac6faab66fb5622
HOTH\Matt Juffs <matthew.juffs@gmail.com>
2020-07-23 16:37:04 +0100

    Added Data Project - .NET Core Library to handle data access

---
+++ Data/Entities/Account.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Entities
+{
+    public class Account
+    {
+        public int AccountID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ Data/Entities/MeterReading.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Entities
+{
+    public class MeterReading
+    {
+        public int MeterReadingID { get; set; }
+        public int AccountID { get; set; }
+        public DateTime MeterReadingDateTime { get; set; }
+        public int MeterReadValue { get; set; }
+    }
+}
